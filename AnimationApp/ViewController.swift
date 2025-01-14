5933a9f4aac55f4b85522efcf521193a867acc14
Tadevos Kurdoglian <tazman311@gmail.com>
2025-01-15 01:22:16 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Тадевос Курдоглян on 15.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
